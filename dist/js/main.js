d768749701243f040a6de18ce1fd370a3bb350cb
Christopher Basketfield <chrisBB@ak.net>
2019-01-10 09:27:17 -0900

    cleaned up scss and html, fixed dot active state, centered slideshow

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,18 +3,23 @@ function showSlides(n) {
   var slides = document.getElementsByClassName("slide");
   var dots = document.getElementsByClassName("dot");
 
+  // SELECTING THE SLIDE BASED ON INPUT
   if (n > slides.length) {
     slideIndex = 1;
   }
   if (n < 1) {
     slideIndex = slides.length;
   }
+
+  //HIDING EVERY SLIDE AND TURNING OFF EVERY DOT
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+
+  //SHOWING THE CURRENT SLIDE AND ACTIVATING THE DOT
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
